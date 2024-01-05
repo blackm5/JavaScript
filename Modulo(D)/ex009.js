55c161504aac4c0b3d228d7f1f265a0eb38a2f22
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-01-05 19:34:44 -0300

    ex009
    Condição Composta

---
+++ Modulo(D)/ex009.js
@@ -0,0 +1,8 @@
+var speed = 501
+console.log(`The speed of your aircreft is ${speed}kt.`)
+if (speed > 500) {
+    console.log(`You overtook the speed of 500kt. Fined!`)
+}
+else {
+    console.log(`Fly ever with security!`)
+}
