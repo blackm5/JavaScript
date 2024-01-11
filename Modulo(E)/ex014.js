d033e7bff2051e48959252e7472f67b293948c64
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-01-11 17:50:52 -0300

    Módulo E/ ex014
    Repetição com teste no INÍCIO e FINAL

---
+++ Modulo(E)/ex014.js
@@ -0,0 +1,10 @@
+var c = 1
+while (c <= 5) {
+    console.log(`${c}`) // Repetição com teste no INÍCIO
+    c++
+}
+do {
+    console.log(`${c}`) // Repetição com teste no FINAL
+    c--
+}
+while (c >= 0)
