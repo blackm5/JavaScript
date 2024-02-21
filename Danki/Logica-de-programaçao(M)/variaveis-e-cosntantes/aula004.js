13144801788681e5336066675d530cd1d271e85b
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-02-21 19:46:49 -0300

    aula004
    Condições

---
+++ Danki/Logica-de-programaçao(M)/variaveis-e-cosntantes/aula004.js
@@ -0,0 +1,34 @@
+//Condições
+var n = 3;
+if(n < 5){
+    console.log(false);
+}
+else if (n > 5){
+    console.log(false);
+}
+else{
+    console.log(true);
+}
+
+var b = 7;
+if (b<7 || b>7){
+    console.log(false);
+}else{
+    console.log(true)
+}
+
+var t = 9
+if (t!=9){
+    console.log(false);
+}else{
+    console.log(true)
+}
+/*
+    > - maior
+    < - menor 
+    >= - maior ou igual
+    <= - menor ou igual
+    || - ou 
+    && - e
+    != - Diferente
+ */
