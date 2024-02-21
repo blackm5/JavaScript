91f60ccd987b38d2516e0444d581cf34d48a843b
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-02-21 18:49:16 -0300

    aula003
    Arrays

---
+++ Danki/Logica-de-programaçao(M)/variaveis-e-cosntantes/aula003.js
@@ -0,0 +1,13 @@
+var ege = new Array ('jake', 'jhon', 'mike');
+console.log(ege);
+
+var name = ['Jake','Jhon', 'Mike'];
+
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+name.push('Cris');
+console.log(name[3]);
+name[0]= 'Cris';
+name[3]= 'Jake';
+console.log(name);
