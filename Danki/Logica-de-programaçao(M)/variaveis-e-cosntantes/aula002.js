4b189f0b4e8ff2c45c76ed6412e2e384f17a9b8f
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-02-21 18:28:49 -0300

    aula002
    Variáveis

---
+++ Danki/Logica-de-programaçao(M)/variaveis-e-cosntantes/aula002.js
@@ -0,0 +1,23 @@
+/*
+
+var = variavel GLOBAL; 
+Ex.: 
+var name = 'Mike'
+var number = Number(document.prompt(Number:).value)
+
+
+let = variavel no ESCOPO;
+{}--> Escopo.
+Ex.: 
+{
+    let ege = 27
+    console.log(ege);
+}
+
+
+const = CONSTANTE;
+Ex.:
+const name = 'Jake'
+console.log(name);
+
+*/
