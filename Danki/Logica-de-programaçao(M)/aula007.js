4d210fe16dff9a803376da45a407f843291a81d4
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-02-21 21:16:58 -0300

    aula007
    forEach: para interagir com Arrays ou Objects.

---
+++ Danki/Logica-de-programaçao(M)/aula007.js
@@ -0,0 +1,9 @@
+var name = ['Jeke', 'Newton', 'Issac'];
+/*
+for (var  i = 0; i < name.length; i++){
+    console.log(name[i]);
+}*/
+
+name.forEach(function(value, index){
+    console.log(index);
+})
