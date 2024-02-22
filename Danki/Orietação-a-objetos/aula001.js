f06b5c3be8be54b3e7c7624aa6a19324dffc95b0
blackm5 <125949105+blackm5@users.noreply.github.com>
2024-02-22 18:34:08 -0300

    Orientação a Objetos/ aula001
    Objetos

---
+++ Danki/Orietação-a-objetos/aula001.js
@@ -0,0 +1,18 @@
+var object = [{
+    'name': 'Mike',
+    'old': 23,
+    'profession': 'programmer',
+}, 
+{
+    'name': 'Jhon',
+    'old': 27,
+    'profession': 'teacher',
+}, 
+];
+console.log(object[0].profession)
+
+var news = [{
+    'title': 'News',
+    'text': 'Content'
+}];
+console.log(news[0].title)
